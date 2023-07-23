5a85c6f146fe2b55d0804af47a72cb2ac0f5f23a
TeemoKill <1158898301@qq.com>
2023-07-24 00:42:13 +0800

    introduced episode ID mapping

--- src/components/pages/item_detail_page.tsx
+++ src/components/pages/item_detail_page.tsx
@@ -3,10 +3,7 @@
 import { HttpStatusCode } from "axios"
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
-
-const episodeIdToNameMap: { [id: number]: string } = {
-
-}
+import { episodeIdMap } from "@/constants/episode_id_map"
 
 interface MatrixResponse {
   error_code: number,
@@ -26,21 +23,25 @@ export default function PavlovItemDetailPage() {
         const response = await fetch(`https://backend.stpavlov-data.com/api/matrix?items=${item_id}`)
         const responseData = await response.json()
         responseData.data.sort(
-          (x: Matrix, y: Matrix) => (y.quantity / y.replay_count) - (x.quantity / x.replay_count)
+          (x: Matrix, y: Matrix) => 
+          episodeIdMap[x.episode_id].cost / (x.quantity / x.replay_count) - episodeIdMap[y.episode_id].cost / (y.quantity / y.replay_count)
         )
 
         setMatrixElements(
           responseData.data.map((d: Matrix) => (
             <div
+              className="flex items-start justify-between"
               style={{
-                justifyItems: "flex-start",
+                width: "80%",
                 border: "1px solid #000000"
               }}
             >
               <h2>
-                关卡：{d.episode_id}
+                关卡：{episodeIdMap[d.episode_id].name} {d.episode_id > 20000 ? "厄险" : "普通"}
               </h2>
+              <h3>关卡ID: {d.episode_id}</h3>
               <div>掉率：{((d.quantity / d.replay_count) * 100).toFixed(2)}%</div>
+              <div>期望活性：{(episodeIdMap[d.episode_id].cost / (d.quantity/d.replay_count)).toFixed(2)}</div>
             </div>
           ))
         )
@@ -54,11 +55,13 @@ export default function PavlovItemDetailPage() {
   }, [])
 
   return (
-    <div>
+    <div className="w-full flex flex-col justify-center">
       {/* TODO: fill up ItemDetailPage */}
       item id: {item_id}
-      <div>
-        {matrixElements}
+      <div className="flex flex-col justify-center">
+        {matrixElements.length > 0 ? 
+        matrixElements 
+        : <h1>No data</h1>}
       </div>
     </div>
   )
