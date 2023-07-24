edefbf5992029573c587157b3b28e6de8960c47f
TeemoKill <1158898301@qq.com>
2023-07-25 05:49:19 +0800

    minor beautification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,16 @@ export default function Page() {
   return (
     <div className="w-full flex flex-col items-center">
 
-      <div className="relative flex place-items-center z-[-1]">
+      <div className="flex place-items-center">
         <Image
           src="https://s3api.wanz.site/image/reverse1999.jpg"
           alt="Saint Pavlov Data Department"
           width={256}
           height={256}
           priority
+          style={{
+            borderRadius: 4,
+          }}
         />
       </div>
 
@@ -28,7 +31,12 @@ export default function Page() {
 
 function ConstructingNote() {
   return (
-    <div className="w-full my-5 p-4 text-center bg-orange-400 text-white box-border top-0 z-99">
+    <div className="w-full my-5 p-4 text-center bg-orange-400 text-white box-border top-0 z-99"
+    style={{
+      fontSize: 14,
+      textShadow: "0px 0px 5px rgb(255,255,255)"
+    }}
+    >
       <p>
         基金会数据部的重建工作正在进行中……
       </p>
@@ -42,10 +50,10 @@ function ConstructingNote() {
 function GithubButton() {
   return (
     <Link href="https://github.com/orgs/St-Pavlov-Data-Department/repositories" target="_blank"
+    className="bg-neutral-700 hover:bg-neutral-500"
       style={{
         display: "inline-flex",
         alignItems: "center",
-        backgroundColor: "#333",
         color: "#fff",
         padding: "10px 20px",
         borderRadius: "8px",
--- src/components/fixed_header.tsx
+++ src/components/fixed_header.tsx
@@ -1,23 +1,28 @@
+import Link from "next/link";
+
 export function FixedHeader() {
   return (
     <div className="flex fixed top-0 left-0 right-0 w-full justify-between items-center bg-gray-900 bg-opacity-50 text-white backdrop-blur-8 px-5 py-2 font-sans text-center lg:flex"
-    style={{
-      zIndex: 255,
-    }}
+      style={{
+        backdropFilter: "blur(4px)",
+        zIndex: 255,
+      }}
     >
-      <div className="header-title flex text-2xl font-bold justify-start items-center w-full">
+      <div className="header-title flex text-2xl font-bold justify-start items-center w-full overflow-hidden whitespace-nowrap">
         St.Pavlov Data Department
       </div>
-      <div className="button bg-orange-300 hover:bg-orange-200 text-white font-bold py-2 px-4 rounded-2xl shadow-lg">
-        <a
-          className="flex place-items-center lg:pointer-events-auto lg:p-0"
-          href="https://backend.stpavlov-data.com/api/doc"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          API Document
-        </a>
-      </div>
+      <Link
+        className="flex place-items-center py-2 px-4 rounded-2xl bg-orange-300 hover:bg-orange-200 lg:pointer-events-auto lg:p-0"
+        href="https://backend.stpavlov-data.com/api/doc"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          textShadow: "2px 2px 4px rgba(0,0,0,0.5)",
+          transition: "background-color 0.2s ease",
+        }}
+      >
+        API Document
+      </Link>
     </div>
   )
 }
